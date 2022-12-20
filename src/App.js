c393cba08d35273d966dfa83cde164c62cb32677
Adarsh <adarshkumar167@gmail.com>
2022-12-20 16:32:44 +0530

    40

--- src/App.js
+++ src/App.js
@@ -10,15 +10,17 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((users) =>
       this.setState(
         () => {
-          return { monsters : users };
+          return { monsters: users };
         },
         () => {
           console.log(this.state);
@@ -28,8 +30,25 @@ class App extends Component {
   }
   
   render() {
+    console.log('render');
       return (
       <div className="App">
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event) => { 
+          console.log(event.target.value);
+          const searchString = event.target.value.toLocaleLowerCase();
+          // [ {name : 'Adarsh}, {name : 'Kumar'}]
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchString);
+          });
+
+          this.setState(() => {
+            return {monsters : filteredMonsters};
+          })
+        }}/>
         {this.state.monsters.map((monster) => {
           return <div key={monster.id}><h1>{monster.name}</h1></div>
         })}
